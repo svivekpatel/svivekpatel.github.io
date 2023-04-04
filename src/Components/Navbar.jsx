267eaf13a702c1e48242630c93b28c7dc8acb509
Vivek Patel <vp133202@gmail.com>
2023-04-04 16:01:18 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar() {
             </HStack>
           </HStack>
           <Flex alignItems={'center'}>
-            <a id="resume-link-1" href="Vivek-Patel-Resume.pdf" target="_blank" download>
+            <a id="resume-link-1" className="nav-link resume" href="Vivek-Patel-Resume.pdf" target="_blank" download>
               <Button id="resume-button-1" color={"black"} onClick={handleResume}>Resume</Button>
             </a>
           </Flex>
